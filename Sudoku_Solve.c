ceced2278bdc8536d0de56150921d8a3bc8cb778
ELcept <eric20060613@163.com>
2025-09-05 11:37:59 +0800

    Add files via upload

---
+++ Sudoku_Solve.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <math.h>
+#include "definition.h"
+#include "cnf_parser.c"
+#include "dpll_solver.c"
+#include "X-Sukodu.c"
+#include "display.c"
+
+
+int main(void){
+    interact();
+    return 0;
+}
+
+
+
+
